1828845a5f4c7e81f267b013a97b6411e036357e
Vincent Muller <v.muller55000@gmail.com>
2023-03-02 12:28:12 +0100

    media factory + photographHeader factory + css photograph page

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,10 +1,9 @@
 function photographerHeaderFactory (target, data) {
     target.innerHTML = `
-        <div>
+        <div class="photographInfo">
             <h2 class="test">${data.name}</h2>
             <h3>${data.city} ${data.country}</h3>
             <p>${data.tagline}</p>
-            <p class="price">${data.price}€/jour</p>
         </div>
         <div id="bloc-contact">
             <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
@@ -13,4 +12,50 @@ function photographerHeaderFactory (target, data) {
             <img src="assets/photographers/${data.portrait}" alt="photo de ${data.name}">
         </div>
     `
+    photographerBannerFactory();
+}
+
+async function photographerBannerFactory() {
+    const totalLikes =  await getTotalLikes();
+    const photographerInfo = await getPhotographerInfo();
+    const target = document.getElementById("banner");
+    target.innerHTML = `
+        <div class="likes">
+            <p>${totalLikes}</p>
+            <img src="./assets/icons/heart-solid.svg">
+        </div>
+        <div class="price">
+            <p>${photographerInfo.price}€ / jour</p>
+        </div>
+    `
+}
+
+/**
+ * 
+ * @param {*} data 
+ */
+
+function mediaFactory(target, mediaData, photographer) {
+    target.innerHTML = mediaData.map((media) => `
+        <article class="media-card"> 
+            <div class = "bloc-img">
+                ${
+                media.image
+                    ? `<img class="for-zoom" src="assets/images/media/${photographer.name}/${media.image}" alt="photo de ${photographer.name}-${media.title}"></img>`
+                    : `<video class="for-zoom"><source src="assets/images/media/${photographer.name}/${media.video}" type="video/mp4" alt="vidéo de ${photographer.name}"></video>`
+                }
+            </div>
+            <div class="mediaInfo">
+                <div class="mediaTittle">
+                    <p>${media.title}</p>
+                </div>
+                <div id="mediaLikesAndPrice">
+                    <p>${media.likes}</p>
+                    <img src="./assets/icons/heart-solid-red.svg" alt="bouton like en forme de coeur">
+                </div>
+            </div>
+        </article>
+    `
+    )
+    .join('')
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,22 +10,50 @@ async function getPhotographers() {
 
 async function getPhotographerInfo(){
     const photographer = await getPhotographers();
-    console.log(photographer.photographers)
     const photographerInfo = photographer.photographers.find(
-        (e) => e.id === id
+        (e) => e.id === +id
     )
-    console.log(photographerInfo)
     return photographerInfo
 }
 
 async function photographerHeader(){
     const photographerInfo = await getPhotographerInfo();
-    const photographerHeader = document.getElementById('photograph-header');
+    const photographerHeader = document.querySelector('.photograph-header');
     photographerHeaderFactory(photographerHeader, photographerInfo);
+}
+
+async function getMediaOfPhotographer(){
+    const medias = await getPhotographers();
+    const mediaOfPhotographer = medias.media.filter(
+        (e) => e.photographerId === +id
+    )
+    return mediaOfPhotographer;
+}
+
+async function sortByLike(){
+    const media = await getMediaOfPhotographer();
+    return media.sort((a, b) => b.likes - a.likes)
+}
+
+async function displayMedia(media){
+    const mediaSection = document.querySelector('.media_section');
+    const photographerInfo = await getPhotographerInfo()
+    media = media ?? (await sortByLike())
+    mediaFactory(mediaSection, media, photographerInfo)
+}
+
+async function getTotalLikes(){
+    const medias = await getMediaOfPhotographer();
+    let likes = 0;
+    for (let i = 0; i < medias.length; i++) {
+        likes += medias[i].likes;
+    }
+    return likes
 }
 
 function init() {
     photographerHeader();
+    displayMedia();
 }
 
 init();
